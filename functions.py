45a7a30f54b80fbcae65ad80badaa37d834a2027
Michael Orecchia <michael.orecchia@gmail.com>
2024-11-17 17:13:22 -0800

    add random sample function

--- functions.py
+++ functions.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import dask.dataframe as dd
 
+
 def count_csv_rows(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         # Count the number of lines in the file
         row_count = sum(1 for _ in file)
     # Subtract 1 for the header row (if present)
     return row_count - 1  # Adjust if there's no header
 
+
 def count_csv_rows_dask(file_path, header=True):
-    ddf = dd.read_csv(file_path, dtype='object')
+    ddf = dd.read_csv(file_path, dtype="object")
     row_count = ddf.shape[0].compute()
-    return row_count if header else row_count # Subtract 1 for the header row (if present)
+    return (
+        row_count if header else row_count
+    )  # Subtract 1 for the header row (if present)
 
 
 def get_unique_values_by_column(file_path, column_name):
@@ -20,16 +24,30 @@ def get_unique_values_by_column(file_path, column_name):
 
     for chunk in pd.read_csv(file_path, usecols=[column_name], chunksize=chunk_size):
         unique_values.update(chunk[column_name].dropna().unique())
-    
+
     return unique_values
+
 
 def get_column_names(file_path):
     # Read only the header row
     column_names = pd.read_csv(file_path, nrows=0).columns.tolist()
     return column_names
 
+
 def display_top_rows(file_path, n):
     # Read the file as a Dask DataFrame
-    ddf = dd.read_csv(file_path, dtype='object')
+    ddf = dd.read_csv(file_path, dtype="object")
     # Compute and fetch the first n rows
     print(ddf.head(n))
+
+
+def get_random_sample(file_path, fraction=0.01, output_path=None, seed=42):
+    ddf = dd.read_csv(file_path, dtype="object")
+
+    sampled_df = ddf.sample(frac=fraction, random_state=seed)
+
+    return (
+        sampled_df.compute()
+        if output_path == None
+        else sampled_df.to_csv(output_path, single_file=True, index=False)
+    )
--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import time
-from functions import count_csv_rows,count_csv_rows_dask, get_column_names, get_unique_values_by_column, display_top_rows
+from functions import (
+    count_csv_rows,
+    count_csv_rows_dask,
+    get_column_names,
+    get_unique_values_by_column,
+    display_top_rows,
+    get_random_sample,
+)
 
-file_path = 'test_data/2018_Yellow_Taxi_Trip_Data.csv'
+file_path = "test_data/2020_Yellow_Taxi_Trip_Data.csv"
 
-column_names = get_column_names(file_path)
-print(f"Column names: {column_names}")
+# column_names = get_column_names(file_path)
+# print(f"Column names: {column_names}")
 
-unique_values = get_unique_values_by_column(file_path, 'VendorID')
-print(f"Unique values: {str(unique_values)}")
+# unique_values = get_unique_values_by_column(file_path, "VendorID")
+# print(f"Unique values: {str(unique_values)}")
 
 # display_top_rows(file_path, 10)
 
 # p_start_time = time.time()
-# rows_pandas = count_csv_rows(file_path)
+rows_pandas = count_csv_rows(file_path)
 # pandas_time = time.time() - p_start_time
 
-# print(f"Total rows (Pandas): {rows_pandas}")
+print(f"Total rows: {rows_pandas}")
 # print(f"Time (Pandas): {pandas_time}")
 
 # # Time the Dask approach
@@ -25,3 +32,5 @@ print(f"Unique values: {str(unique_values)}")
 
 # print(f"Total rows (Dask): {rows_dask}")
 # print(f"Time (Dask): {dask_time}")
+sample = get_random_sample(file_path)
+print(sample)
