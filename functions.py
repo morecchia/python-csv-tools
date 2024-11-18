2ce49cd6ef23593b4fbe0f5189178fd74af6dfda
Michael Orecchia <michael.orecchia@gmail.com>
2024-11-17 16:47:56 -0800

    add dask example

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import dask.dataframe as dd
 
 def count_csv_rows(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -7,8 +8,13 @@ def count_csv_rows(file_path):
     # Subtract 1 for the header row (if present)
     return row_count - 1  # Adjust if there's no header
 
+def count_csv_rows_dask(file_path, header=True):
+    ddf = dd.read_csv(file_path, dtype='object')
+    row_count = ddf.shape[0].compute()
+    return row_count if header else row_count # Subtract 1 for the header row (if present)
 
-def get_unique_values(file_path, column_name):
+
+def get_unique_values_by_column(file_path, column_name):
     chunk_size = 10**6  # Number of rows per chunk
     unique_values = set()
 
@@ -21,3 +27,9 @@ def get_column_names(file_path):
     # Read only the header row
     column_names = pd.read_csv(file_path, nrows=0).columns.tolist()
     return column_names
+
+def display_top_rows(file_path, n):
+    # Read the file as a Dask DataFrame
+    ddf = dd.read_csv(file_path, dtype='object')
+    # Compute and fetch the first n rows
+    print(ddf.head(n))
---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+from functions import count_csv_rows,count_csv_rows_dask, get_column_names, get_unique_values_by_column, display_top_rows
+
+file_path = 'test_data/2018_Yellow_Taxi_Trip_Data.csv'
+
+column_names = get_column_names(file_path)
+print(f"Column names: {column_names}")
+
+unique_values = get_unique_values_by_column(file_path, 'VendorID')
+print(f"Unique values: {str(unique_values)}")
+
+# display_top_rows(file_path, 10)
+
+# p_start_time = time.time()
+# rows_pandas = count_csv_rows(file_path)
+# pandas_time = time.time() - p_start_time
+
+# print(f"Total rows (Pandas): {rows_pandas}")
+# print(f"Time (Pandas): {pandas_time}")
+
+# # Time the Dask approach
+# d_start_time = time.time()
+# rows_dask = count_csv_rows_dask(file_path)
+# dask_time = time.time() - d_start_time
+
+# print(f"Total rows (Dask): {rows_dask}")
+# print(f"Time (Dask): {dask_time}")
